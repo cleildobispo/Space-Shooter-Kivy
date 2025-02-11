c209ed6ab9a5efd76adb5f619208dccbced6b321
cleildobispo <96308347+cleildobispo@users.noreply.github.com>
2025-02-11 15:21:21 -0300

    Update Space Invader.py

--- Space Invader.py
+++ Space Invader.py
@@ -6,7 +6,9 @@ from kivy.graphics import Rectangle, Color
 from kivy.clock import Clock    #atualização da tela em tempo real
 from kivy.properties import ListProperty, OptionProperty
 from random import randint
+from kivy.uix.label import Label
 import logging
+
 
 screen_width = 800
 screen_height = 600
@@ -38,33 +40,37 @@ class Inimigo(Image):
        
             
 class SpaceInvaderGame(Widget):
-    tiros = ListProperty([])
-    inimigos = ListProperty([])
-    game_overimage = OptionProperty('gameover.png')
-    game_over_flag = False
-    
-    
-    def __init__(self, **kwargs):
-        
-        super(SpaceInvaderGame, self).__init__(**kwargs)
 
+    game_overimage = OptionProperty('gameover.png')
+
+    
+   
+    def __init__(self, **kwargs): 
+        super(SpaceInvaderGame, self).__init__(**kwargs)
+        self.tiros = []
+        self.inimigos = []
+        self.game_over_flag = False
         self.logger = logging.getLogger().getChild(__name__)
+        self.pontuacao = 0
         
         with self.canvas:
             Color(0, 0, 0, 1)
             self.rect = Rectangle(pos=(0,0), size=(screen_width, screen_height))
             self.background = Image(source='./fundo.png', pos=(0,0), size = Window.size, fit_mode="fill")
-                               
+                   
         # adicionando a imagem da nave,  com a posiçõa
         self.nave = Image(source='./nave.png', size=(74,74), pos=(screen_width/2-50, 20), allow_stretch = True)
         self.add_widget(self.nave)
+        
+        self.label_pontuacao = Label(text=f'Pontos: {self.pontuacao}', pos=(50, 500), size_hint=(None, None), font_size='30sp', color=(1, 1, 1, 1), outline_color = (0, 0, 0, 1), outline_width = 2)
+        self.add_widget(self.label_pontuacao, index=0)
+        
         Window.bind(on_resize=self.window_resize)
-
         self.logger.info("Testing Logger, init finished")
         
      # movimentao da nave  
         Window.bind(on_key_down = self.IrDireita)
-        Window.bind(on_key_up = self.IrEsquerda)    
+        Window.bind(on_key_up = self.IrEsquerda)
         
         self.left_pressed = False
         self.right_pressed = False
@@ -83,8 +89,6 @@ class SpaceInvaderGame(Widget):
             self.left_pressed = True
         if key == 275:
             self.right_pressed = True
-        if key == 32: #aqui é pra atirar qnd apertar o espaço 
-            self.atirar()
                 
     def IrEsquerda(self, window, key, *args):
         if key == 276:
@@ -92,12 +96,19 @@ class SpaceInvaderGame(Widget):
         if key == 275:
             self.right_pressed = False
         if key == 32:
-            self.atirar()
+            if self.game_over_flag:
+                self.game_over_flag = False
+                self.inimigos = []
+                self.tiros = []
+                App.get_running_app().restart()
+                
+            else:
+                self.atirar()
             
+                
     def update(self, dt):
         if self.game_over_flag:
             return
-        
         if self.left_pressed and self.nave.x > 0:
             self.nave.x -= 5
         if self.right_pressed and self.nave.right < self.width:
@@ -107,15 +118,14 @@ class SpaceInvaderGame(Widget):
             if bullet.y > screen_height:
                 self.remove_widget(bullet)
                 self.tiros.remove(bullet) 
-                
             bullet.update()
-        for enemy in self.inimigos:
             
+        for enemy in self.inimigos:
             enemy.y -= 1
             if enemy.y < 0:
                 self.remove_widget(enemy)
                 self.inimigos.remove(enemy)       
-                
+                 
         self.ver_colisao()
         
     def atirar(self):
@@ -138,46 +148,55 @@ class SpaceInvaderGame(Widget):
          while(len(self.inimigos) < 15):
             enemy = Inimigo()
             enemy.pos = (randint(0, screen_width - enemy.width), screen_height)
-            self.add_widget(enemy)
+            self.add_widget(enemy, index = 10)
             self.inimigos.append(enemy) 
     def ver_colisao(self):
         for enemy in self.inimigos:
             for bullet in self.tiros:
                 if self.colisao(bullet, enemy):
-                    self.remove_widget(bullet)
-                    self.remove_widget(enemy)
                     self.tiros.remove(bullet)
                     self.inimigos.remove(enemy)
-                    break
-        for enemy in self.inimigos[:]:
+                    self.remove_widget(bullet)
+                    self.remove_widget(enemy)
+                    self.pontuacao += 1
+                    self.label_pontuacao.text = f"Pontos: {self.pontuacao}"
+                          
+        for enemy in self.inimigos:
             if self.colisao(enemy, self.nave):
                 self.game_over() 
-            
+                break                
+            if enemy.y == self.nave.y:
+                self.game_over()
         
-              
     def colisao(self, obj1, obj2):
-        if (obj1.x < obj2.right and obj1.right > obj2.x and
-            obj1.y < obj2.top and obj1.top > obj2.y):
+        if obj1.collide_widget(obj2):
             return True
         return False
      
-def game_over(self):
-    self.game_over_flag = True
-    for bullet in self.tiros:
-        self.remove_widget(bullet)
-    for enemy in self.inimigos:
-        self.remove_widget(enemy)
-    self.bullets.clear()
-    self.enemies.clear()    
-    self.game_over_image = source='gameover.png'       
-    self.game_over.image.size = (400, 400)
-    self.add_widget(self.game_over_image)
-    self.game_over_image.pos = (screen_width/2 - 200, screen_height/2 - 200)
-    self.add_widget(self.game_over_image)
-    
+    def game_over(self): 
+        self.inimigos = []
+        self.tiros = []
+        self.game_over_flag = True
+        self.game_over_image = Image(source='./gameover.png', pos=(screen_width/2 - 200, screen_height/2 - 200), size = (400, 400), fit_mode="fill")
+        self.add_widget(self.game_over_image)
+        # if key == 32:
+        #     self.game_over_flag = False
+        #     self.inimigos = []
+        #     self.tiros = []
+        #     App.get_running_app().restart()
+        
 class SpaceInvaderApp(App):
+    def restart(self):
+        Window.remove_widget(self.root)
+        new_root = SpaceInvaderGame()
+        Window.add_widget(new_root)
+        self.root = new_root
+        # return SpaceInvaderApp().run()
+        
     def build(self):
         return SpaceInvaderGame()
 
 if __name__ == '__main__':
     SpaceInvaderApp().run() 
+    
+    
